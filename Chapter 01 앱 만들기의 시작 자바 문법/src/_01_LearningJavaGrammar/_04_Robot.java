1a10a69ac306306b401d088eae50f289a836050a
Picik-picik <56762198+Picik-picik@users.noreply.github.com>
2020-02-10 03:47:48 +0900

    자간 수정 200210 월요일

--- Chapter 01 앱 만들기의 시작 자바 문법/src/_01_LearningJavaGrammar/_04_Robot.java
+++ Chapter 01 앱 만들기의 시작 자바 문법/src/_01_LearningJavaGrammar/_04_Robot.java
@@ -1,17 +1,17 @@
 package _01_LearningJavaGrammar;
 public class _04_Robot {
 
-	int x, y;		// 멤버 변수
+	int x, y;	// 멤버 변수
 	int power;		
 	int price;		
 	String name;	// 문자열
 	
 	_04_Robot(int x, int y, int price, String name) {
-		this.x = x;				// 생성자
-		this.y = y;				// 로봇의 좌표 x, y와 로봇의 가격 price, 로봇의 이름을 매개변수로
-		this.price = price;	// 받는 생성자이다. 여기서 참조 변수 this를 써준 것은 넘겨 받은
-		this.name = name;	// 매개변수 x, y, price, name 값이 아니라
-	}							// 멤버 변수인 x, y, price, name을 의미한다.
+		this.x = x;		// 생성자
+		this.y = y;		// 로봇의 좌표 x, y와 로봇의 가격 price, 로봇의 이름을 매개변수로 받는 생성자이다.
+		this.price = price;	// 여기서 참조 변수 this를 써준 것은 넘겨 받은 매개변수 x, y, price, name 값이 아니라
+		this.name = name;	// 멤버 변수인 x, y, price, name을 의미한다.
+	}
 	      
 	void moveRight() {
 		x += 5;		// x 값을 5만큼 증가시키기, x = x + 5 와 같다.	
